10ad44fb1e92bc6775413ee241f127850dca3a0e
Marcelo Cesar dos Santos <marcelocsantos7@gmail.com>
2026-07-26 20:24:35 -0300

    comment code on pointers

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -6,11 +6,20 @@ import (
 
 func main() {
 	age := 32
+	agePointer := &age // cria o pointero para variavel age
 
-	fmt.Println("Age:", age)
-	fmt.Println("Adult years:", getAdultYears(age))
+	/*
+		em uma variavel normal, usa o &var
+		para obter o endereço da memória da variável
+
+		E *var para DESREFERENCIAR o ponteiro e usar o valor
+		no endereço da variável
+	*/
+
+	fmt.Println("Age:", *agePointer) // desreferenciar => usar o VALOR no endereço da mémoria
+	fmt.Println("Adult years:", getAdultYears(agePointer))
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+func getAdultYears(age *int) int {
+	return *age - 18
 }
