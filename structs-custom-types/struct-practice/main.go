9577a7e28dcb441d911d9c61372aa3da3cbb4a7b
marcelo cesar <marcelocsantos7@gmail.com>
2026-08-03 15:57:34 -0300

    starting with interfaces

--- structs-custom-types/struct-practice/main.go
+++ structs-custom-types/struct-practice/main.go
@@ -7,32 +7,53 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
+
+type saver interface {
+	Save() error
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+	if err != nil {
+		fmt.Println("Saving the note failed")
+		return err
+	}
+	fmt.Println("Saving succeeded")
+	return nil
+}
 
 func getNoteData() (string, string) {
 	fmt.Println("Notes")
-	title := getUserInputt("Note title: ")
-	content := getUserInputt("Note content: ")
+	title := getUserInput("Note title: ")
+	content := getUserInput("Note content: ")
 	return title, content
 }
 
 func main() {
 	title, content := getNoteData()
+	todoText := getUserInput("Todo text: ")
+
+	todo, err := todo.New(todoText)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	todo.Print()
+	saveData(todo)
+
 	userNote, err := note.New(title, content)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 	userNote.Print()
-	err = userNote.Save()
-	if err != nil {
-		fmt.Println("Saving the note failed")
-		return
-	}
-	fmt.Println("Saving succeeded")
+	saveData(userNote)
 }
 
-func getUserInputt(prompt string) string {
+func getUserInput(prompt string) string {
 	fmt.Printf("%v ", prompt)
 	reader := bufio.NewReader(os.Stdin)
 	text, err := reader.ReadString('\n')
