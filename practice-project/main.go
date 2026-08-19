d52208efe5ba508f4735af2e4818588853a411d6
Marcelo Cesar dos Santos <marcelocsantos7@gmail.com>
2026-08-19 06:53:32 -0300

    iniciando o practice project branch

--- practice-project/main.go
+++ practice-project/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	prices := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+
+	result := make(map[float64][]float64)
+
+	for _, tax := range taxRates{
+		taxIncludedPrice := make([]float64, len(prices))
+		for priceIndex, price := range prices {
+			taxIncludedPrice[priceIndex] = price * (1 + tax)
+		}
+		result[tax] = taxIncludedPrice
+	}
+	fmt.Println(result)
 }
